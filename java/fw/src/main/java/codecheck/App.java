fa35bd2525aef188ea065d79f9304d921b7106d7
senimagan <senimagan@gmail.com>
2017-04-11 15:40:30 +0900

    test

--- java/fw/src/main/java/codecheck/App.java
+++ java/fw/src/main/java/codecheck/App.java
@@ -2,6 +2,7 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
+		System.out.println("DEBUG");
 		for (int i = 0, l = args.length; i < l; i++) {
 			System.out.println(args[i]);
 		}
