258e48de061f95e4a890fc8d9186ff02241037e8
senimagan <senimagan@gmail.com>
2017-04-14 15:45:09 +0900

    test

--- ai.sh
+++ ai.sh
@@ -2,4 +2,12 @@
 
 ROOT=$(cd $(dirname $0) && pwd)
 
-./sample_ai "$@"
+### Java ###
+java -jar $(ls $ROOT/java/ai/target/exam2a-ai-*.jar) "$@"
+
+### Python ###
+# python $ROOT/python/src/exam2a-ai.pyc "$@"
+
+### NodeJS ###
+# cd $(dirname $0)/js
+# node $ROOT/nodejs/src/exam2a-ai.js "$@"
